3bef724336bfab3c581414f2a0097e56bde9f472
AfiraRolobessy03 <81508591+AfiraRolobessy03@users.noreply.github.com>
2022-05-29 22:58:54 +0700

    Create Nomor2.r

---
+++ Nomor2.r
@@ -0,0 +1,15 @@
+#2 a 
+Setuju
+
+#2 b 
+library(BSDA)
+
+tsum.test(
+  mean.x = 23500, 
+  s.x = sd(3900), 
+  n.x = 100
+)
+
+#2c 
+kesimpulan dari soal diatas adalah mobil yang digunakan rata ratanya lebih dari 
+20.000 km/tahun
